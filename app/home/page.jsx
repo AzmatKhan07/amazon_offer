cb197292ade683f1bb2dd4b4bb362271369df205
Azmat Ullah Khan <136077752+AzmatKhan07@users.noreply.github.com>
2024-06-26 21:21:42 +0500

    setup of product page

---
+++ app/home/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Product() {
+  return <div>this is product page</div>;
+}
+
+export default Product;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Product from "./home/page";
 
 export default function Home() {
-  return <h1>hello test</h1>;
+  return (
+    <>
+      <Product />
+    </>
+  );
 }
